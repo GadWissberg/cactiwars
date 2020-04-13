f9426faf4a8d4a893cb3f1106c136e9bb95d88ed
Gad Wissberg <gadw17@gmail.com>
2020-04-14 01:13:57 +0300

    Fixing bugs.

--- core/src/com/gadarts/war/components/CameraComponent.java
+++ core/src/com/gadarts/war/components/CameraComponent.java
@@ -3,41 +3,40 @@ package com.gadarts.war.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool;
 
 public class CameraComponent implements Component, Pool.Poolable {
-    private PerspectiveCamera camera;
-    private float manipulationSpeed;
+	private PerspectiveCamera camera;
+	private Vector2 manipulationSpeed = new Vector2();
+	private Entity target;
 
-    public Entity getTarget() {
-        return target;
-    }
+	public Entity getTarget() {
+		return target;
+	}
 
-    private Entity target;
+	public void setTarget(Entity target) {
+		this.target = target;
+	}
 
-    @Override
-    public void reset() {
-        camera = null;
-    }
+	@Override
+	public void reset() {
+		camera = null;
+	}
 
-    public void setCamera(PerspectiveCamera camera) {
-        this.camera = camera;
-    }
+	public PerspectiveCamera getCamera() {
+		return camera;
+	}
 
-    public PerspectiveCamera getCamera() {
-        return camera;
-    }
+	public void setCamera(PerspectiveCamera camera) {
+		this.camera = camera;
+	}
 
-    public void setTarget(Entity target) {
-        this.target = target;
-    }
+	public Vector2 getManipulationSpeed(Vector2 out) {
+		return out.set(manipulationSpeed);
+	}
 
-
-    public float getManipulationSpeed() {
-        return manipulationSpeed;
-    }
-
-    public void setManipulationSpeed(float manipulationSpeed) {
-        this.manipulationSpeed = manipulationSpeed;
-    }
+	public void setManipulationSpeed(Vector2 manipulationSpeed) {
+		this.manipulationSpeed.set(manipulationSpeed);
+	}
 }
--- core/src/com/gadarts/war/systems/CameraSystem.java
+++ core/src/com/gadarts/war/systems/CameraSystem.java
@@ -6,182 +6,128 @@ import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector3;
 import com.gadarts.shared.SharedC.Camera;
 import com.gadarts.war.DefaultGameSettings;
-import com.gadarts.war.GameC;
 import com.gadarts.war.components.CameraComponent;
 import com.gadarts.war.components.ComponentsMapper;
-import com.gadarts.war.components.character.MovementState;
 import com.gadarts.war.screens.BattleScreen;
 import com.gadarts.war.systems.player.PlayerSystemEventsSubscriber;
-
-import static com.gadarts.war.systems.physics.PhysicsSystem.auxMatrix;
 
 /**
  * Defines the camera's movement and behaviour.
  */
 public class CameraSystem extends GameEntitySystem implements PlayerSystemEventsSubscriber {
-    private static Vector3 auxVector31 = new Vector3();
-    private static Vector3 auxVector32 = new Vector3();
-    private static Vector3 auxVector2 = new Vector3();
-    private CameraInputController debugInputProcessor;
-    private Entity cameraEntity;
+	private final static Vector3 auxVector33 = new Vector3();
+	private static final float CAMERA_Z_RELATIVE_OFFSET = 8;
+	private static final Vector3 auxVector31 = new Vector3();
+	private static final Vector3 auxVector32 = new Vector3();
+	private static final Vector3 auxVector34 = new Vector3();
+	private CameraInputController debugInputProcessor;
+	private Entity cameraEntity;
 
-    public static CameraInputController createAndSetDebugInputProcessor(PerspectiveCamera camera) {
-        CameraInputController processor = new CameraInputController(camera);
-        processor.autoUpdate = true;
-        Gdx.input.setInputProcessor(processor);
-        return processor;
-    }
+	public static CameraInputController createAndSetDebugInputProcessor(PerspectiveCamera camera) {
+		CameraInputController processor = new CameraInputController(camera);
+		processor.autoUpdate = true;
+		Gdx.input.setInputProcessor(processor);
+		return processor;
+	}
 
-    private CameraComponent createCameraComponent(PooledEngine pooledEngine) {
-        CameraComponent cameraComponent = (pooledEngine).createComponent(CameraComponent.class);
-        cameraComponent.setCamera(createCamera());
-        return cameraComponent;
-    }
+	public static PerspectiveCamera createCamera() {
+		PerspectiveCamera cam = new PerspectiveCamera(Camera.FOV, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.near = Camera.NEAR;
+		cam.far = Camera.FAR;
+		cam.update();
+		return cam;
+	}
 
-    public static PerspectiveCamera createCamera() {
-        PerspectiveCamera cam = new PerspectiveCamera(Camera.FOV, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.near = Camera.NEAR;
-        cam.far = Camera.FAR;
-        cam.update();
-        return cam;
-    }
+	private CameraComponent createCameraComponent(PooledEngine pooledEngine) {
+		CameraComponent cameraComponent = (pooledEngine).createComponent(CameraComponent.class);
+		cameraComponent.setCamera(createCamera());
+		return cameraComponent;
+	}
 
-    @Override
-    public void addedToEngine(Engine engine) {
-        super.addedToEngine(engine);
-        cameraEntity = new Entity();
-        PooledEngine pooledEngine = (PooledEngine) engine;
-        CameraComponent cameraComponent = createCameraComponent(pooledEngine);
-        cameraEntity.add(cameraComponent);
-        engine.addEntity(cameraEntity);
-        if (DefaultGameSettings.SPECTATOR)
-            debugInputProcessor = createAndSetDebugInputProcessor(cameraComponent.getCamera());
-    }
+	@Override
+	public void addedToEngine(Engine engine) {
+		super.addedToEngine(engine);
+		cameraEntity = new Entity();
+		PooledEngine pooledEngine = (PooledEngine) engine;
+		CameraComponent cameraComponent = createCameraComponent(pooledEngine);
+		cameraEntity.add(cameraComponent);
+		engine.addEntity(cameraEntity);
+		if (DefaultGameSettings.SPECTATOR)
+			debugInputProcessor = createAndSetDebugInputProcessor(cameraComponent.getCamera());
+	}
 
-    private void followTarget(CameraComponent cameraComponent, float deltaTime) {
-        Entity target = cameraComponent.getTarget();
-        PerspectiveCamera camera = cameraComponent.getCamera();
-        handleCameraManipulation(deltaTime, target);
-        camera.update();
-    }
+	private void followTarget(CameraComponent cameraComponent) {
+		Entity target = cameraComponent.getTarget();
+		PerspectiveCamera camera = cameraComponent.getCamera();
+		handleCameraManipulation(target);
+		camera.update();
+	}
 
-    private void handleCameraManipulation(float deltaTime, Entity target) {
-        Vector3 linearVelocity = ComponentsMapper.physics.get(target).getBody().getLinearVelocity();
-        float linearSpeed = linearVelocity.len2();
-        ComponentsMapper.physics.get(target).getMotionState().getWorldTransform(auxMatrix);
-        if (!linearVelocity.isCollinear(auxVector31.set(0, -1, 0), 0.1f) && Math.abs(linearSpeed) >= 0.1f)
-            handleCameraManOnMovement(deltaTime, target);
-        else handleCameraManOnIdle(deltaTime);
-    }
+	private void handleCameraManipulation(Entity target) {
+		ComponentsMapper.physics.get(target).getBody().getInterpolationLinearVelocity(auxVector34);
+		Vector3 camPos = auxVector31.set(ComponentsMapper.camera.get(cameraEntity).getCamera().position);
+		Vector3 targetPosition = ComponentsMapper.physics.get(target).getMotionState().getWorldTranslation(auxVector32);
+		auxVector33.set(targetPosition.x, camPos.y, targetPosition.z).add(0, 0, CAMERA_Z_RELATIVE_OFFSET);
+		manipulateCamera(auxVector34);
+		camPos.interpolate(auxVector33, 0.1f, Interpolation.circle);
+		ComponentsMapper.camera.get(cameraEntity).getCamera().position.set(camPos);
+	}
 
-    private void handleCameraManOnMovement(float deltaTime, Entity target) {
-        Vector3 targetPos = auxMatrix.getTranslation(auxVector2);
-        CameraComponent cameraComponent = ComponentsMapper.camera.get(this.cameraEntity);
-        Vector3 cameraPosition = cameraComponent.getCamera().position;
-        if (auxVector31.set(cameraPosition).dst2(targetPos) < GameC.Camera.MAX_ZOOM_DISTANCE) {
-            Vector3 directionFromTarget = auxVector32.set(targetPos).sub(cameraPosition).nor();
-            cameraPosition.add(directionFromTarget.scl(-deltaTime));
-        }
-        handleCameraFrontOffset(deltaTime, target, cameraPosition, targetPos);
-    }
+	private void manipulateCamera(Vector3 velocity) {
+		if (Math.abs(velocity.len2()) > 0.01) {
+			velocity.scl(4);
+			auxVector33.add(velocity.x, 0, velocity.z);
+		}
+	}
 
-    private void handleCameraFrontOffset(float deltaTime, Entity target, Vector3 cameraPosition, Vector3 targetPos) {
-        CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
-        cameraComponent.setManipulationSpeed(cameraComponent.getManipulationSpeed() + 0.1f);
-        Vector3 targetDirection = auxVector31.set(1, 0, 0).rot(auxMatrix).nor();
-        float newX = calculateNewXForTargetFollowing(deltaTime, target, targetPos, targetDirection);
-        float newZ = calculateNewZForTargetFollowing(deltaTime, target, targetPos, targetDirection);
-        cameraPosition.set(newX, cameraPosition.y, newZ);
-    }
+	@Override
+	public void update(float deltaTime) {
+		if (BattleScreen.isPaused()) return;
+		super.update(deltaTime);
+		if (debugInputProcessor != null) debugInputProcessor.update();
+		CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
+		if (!DefaultGameSettings.SPECTATOR && cameraComponent.getTarget() != null) {
+			followTarget(cameraComponent);
+		}
+	}
 
-    private float calculateNewXForTargetFollowing(float deltaTime, Entity target, Vector3 targetPos, Vector3 targetDir) {
-        float linearSpeed = ComponentsMapper.physics.get(target).getBody().getLinearVelocity().len2();
-        float minX = targetPos.x - GameC.Camera.MAX_X_FRONT_OFFSET;
-        float maxX = targetPos.x + GameC.Camera.MAX_X_FRONT_OFFSET;
-        MovementState movementState = ComponentsMapper.characters.get(target).getMovementState();
-        boolean isAccelerating = movementState == MovementState.ACCELERATING && linearSpeed > 1f;
-        int coef = isAccelerating ? 1 : -1;
-        CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
-        float manipulationSpeed = cameraComponent.getManipulationSpeed();
-        Vector3 cameraPosition = cameraComponent.getCamera().position;
-        float newX = cameraPosition.x + coef * targetDir.x * Math.min(linearSpeed, manipulationSpeed) * deltaTime;
-        return MathUtils.clamp(newX, minX, maxX);
-    }
+	/**
+	 * Sets the camera to follow the given entity.
+	 *
+	 * @param target
+	 */
+	public void lockToTarget(Entity target) {
+		CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
+		cameraComponent.setTarget(target);
+		Vector3 targetPos = ComponentsMapper.physics.get(target).getMotionState().getWorldTranslation(auxVector31);
+		PerspectiveCamera camera = cameraComponent.getCamera();
+		Vector3 position = camera.position;
+		position.set(targetPos.x + 5, targetPos.y + Camera.TARGET_Y_MIN_OFFSET, targetPos.z + 2 * Camera.TARGET_Z_MIN_OFFSET);
+		camera.up.set(0, 1, 0);
+		camera.direction.rotate(Vector3.X, -45);
+		camera.update();
+	}
 
-    private float calculateNewZForTargetFollowing(float deltaTime, Entity target, Vector3 targetPos, Vector3 targetDir) {
-        CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
-        float linearSpeed = ComponentsMapper.physics.get(target).getBody().getLinearVelocity().len2();
-        float minZ = targetPos.z + GameC.Camera.MIN_Z_FRONT_OFFSET;
-        float maxZ = targetPos.z + GameC.Camera.MAX_Z_FRONT_OFFSET;
-        MovementState movementState = ComponentsMapper.characters.get(target).getMovementState();
-        boolean isAccelerating = movementState == MovementState.ACCELERATING && linearSpeed > 1f;
-        Vector3 cameraPosition = ComponentsMapper.camera.get(cameraEntity).getCamera().position;
-        int coef = isAccelerating ? 1 : -1;
-        float manipulationSpeed = cameraComponent.getManipulationSpeed();
-        float newX = cameraPosition.z + coef * targetDir.z * Math.min(linearSpeed, manipulationSpeed) * deltaTime;
-        return MathUtils.clamp(newX, minZ, maxZ);
-    }
+	@Override
+	public void onMovementAccelerationBegan() {
 
-    private void handleCameraManOnIdle(float deltaTime) {
-        Vector3 targetPos = auxMatrix.getTranslation(auxVector2);
-        Vector3 cameraPosition = ComponentsMapper.camera.get(this.cameraEntity).getCamera().position;
-        if (auxVector31.set(cameraPosition).dst2(targetPos) > GameC.Camera.MIN_ZOOM_DISTANCE) {
-            ComponentsMapper.camera.get(cameraEntity).setManipulationSpeed(0);
-            Vector3 directionFromTarget = auxVector32.set(auxVector31.set(targetPos.x,
-                    targetPos.y + Camera.TARGET_Y_MIN_OFFSET, targetPos.z + Camera.TARGET_Z_MIN_OFFSET))
-                    .sub(cameraPosition).nor();
-            cameraPosition.add(directionFromTarget.scl(deltaTime));
-        }
-    }
+	}
 
-    @Override
-    public void update(float deltaTime) {
-        if (BattleScreen.isPaused()) return;
-        super.update(deltaTime);
-        if (debugInputProcessor != null) debugInputProcessor.update();
-        CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
-        if (!DefaultGameSettings.SPECTATOR && cameraComponent.getTarget() != null) {
-            followTarget(cameraComponent, deltaTime);
-        }
-    }
+	@Override
+	public void onMovementAccelerationStopped() {
 
-    /**
-     * Sets the camera to follow the given entity.
-     *
-     * @param target
-     */
-    public void lockToTarget(Entity target) {
-        CameraComponent cameraComponent = ComponentsMapper.camera.get(cameraEntity);
-        cameraComponent.setTarget(target);
-        Vector3 targetPos = ComponentsMapper.physics.get(target).getMotionState().getWorldTranslation(auxVector31);
-        PerspectiveCamera camera = cameraComponent.getCamera();
-        Vector3 position = camera.position;
-        position.set(targetPos.x, targetPos.y + Camera.TARGET_Y_MIN_OFFSET, targetPos.z + Camera.TARGET_Z_MIN_OFFSET);
-        camera.lookAt(targetPos);
-        camera.up.set(0, 1, 0);
-        camera.update();
-    }
+	}
 
-    @Override
-    public void onMovementAccelerationBegan() {
+	public Entity getCameraEntity() {
+		return cameraEntity;
+	}
 
-    }
+	@Override
+	public void onResize(int width, int height) {
 
-    @Override
-    public void onMovementAccelerationStopped() {
-
-    }
-
-    public Entity getCameraEntity() {
-        return cameraEntity;
-    }
-
-    @Override
-    public void onResize(int width, int height) {
-
-    }
+	}
 }
